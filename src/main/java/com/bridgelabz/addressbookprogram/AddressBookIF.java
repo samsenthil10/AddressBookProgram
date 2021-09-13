09e87189b25ebb2f0bd46487adc1a00897839f70
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-14 04:01:13 +0530

    Added: To search contacts in all city/state

--- src/main/java/com/bridgelabz/addressbookprogram/AddressBookIF.java
+++ src/main/java/com/bridgelabz/addressbookprogram/AddressBookIF.java
@@ -6,6 +6,8 @@ public interface AddressBookIF {
 
 	public AddressBook selectActiveAddressBook(Map<String, AddressBook> addressBooks);
 	public <K, V> String getKey(Map<String, AddressBook> map, AddressBook value);
-	public void showContactInGivenCity(String enteredCity, Map<String, AddressBook> addressBooks);
-	public void showContactInGivenState(String enteredState, Map<String, AddressBook> addressBooks);
+	public void showContactsInGivenCity(String enteredCity, Map<String, AddressBook> addressBooks);
+	public void showContactsInGivenState(String enteredState, Map<String, AddressBook> addressBooks);
+	public void showAllContactsInAllCity(Map<String, AddressBook> addressBooks);
+	public void showAllContactsInAllState(Map<String, AddressBook> addressBooks);
 }
--- src/main/java/com/bridgelabz/addressbookprogram/AddressBookImpl.java
+++ src/main/java/com/bridgelabz/addressbookprogram/AddressBookImpl.java
@@ -1,8 +1,12 @@
 package com.bridgelabz.addressbookprogram;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 
 public class AddressBookImpl implements AddressBookIF {
 
@@ -34,7 +38,7 @@ public class AddressBookImpl implements AddressBookIF {
 	}
 
 	@Override
-	public void showContactInGivenCity(String enteredCity, Map<String, AddressBook> addressBooks) {
+	public void showContactsInGivenCity(String enteredCity, Map<String, AddressBook> addressBooks) {
 
 		int emptyFlag=0;
 		System.out.println("City: "+enteredCity);
@@ -55,7 +59,7 @@ public class AddressBookImpl implements AddressBookIF {
 	}
 
 	@Override
-	public void showContactInGivenState(String enteredState, Map<String, AddressBook> addressBooks) {
+	public void showContactsInGivenState(String enteredState, Map<String, AddressBook> addressBooks) {
 
 		int emptyFlag=0;
 		System.out.println("State: "+enteredState);
@@ -71,5 +75,64 @@ public class AddressBookImpl implements AddressBookIF {
 		}
 		if(emptyFlag == 0)
 			System.out.println("No Contacts Found!");
+	}
+
+	@Override
+	public void showAllContactsInAllCity(Map<String, AddressBook> addressBooks) {
+		int emptyFlag =0;
+		Multimap<String, String> cityContacts = ArrayListMultimap.create();
+		for (Entry<String, AddressBook> entry : addressBooks.entrySet()) {
+			Iterator<Contacts> iterator = entry.getValue().contacts.iterator();
+			while(iterator.hasNext()) {
+				Contacts contact = iterator.next();
+				if(contact == null)
+					continue;
+				cityContacts.put(contact.getCity(),contact.getFirstName()+" "+contact.getLastName());
+			}					
+		}
+		for(Entry<String,Collection<String>> result : cityContacts.asMap().entrySet()) {
+			if(result.getValue().size()==0 && result.getKey()!=null) {
+				System.out.println(result.getKey()+": No Contacts Found");
+				break;
+			}
+
+			else if(result.getKey()!=null) {
+				emptyFlag=1;
+				System.out.println(result.getKey()+": "+result.getValue());
+			}
+		}
+		if(emptyFlag == 0) {
+			System.out.println("No Records Found");
+		}
+	}
+
+	@Override
+	public void showAllContactsInAllState(Map<String, AddressBook> addressBooks) {
+
+		int emptyFlag=0;
+		Multimap<String, String> stateContacts = ArrayListMultimap.create();
+		for (Entry<String, AddressBook> entry : addressBooks.entrySet()) {
+			Iterator<Contacts> iterator = entry.getValue().contacts.iterator();
+			while(iterator.hasNext()) {
+				Contacts contact = iterator.next();
+				if(contact == null)
+					continue;
+				stateContacts.put(contact.getState(),contact.getFirstName()+" "+contact.getLastName());
+			}					
+		}
+		for(Entry<String,Collection<String>> result : stateContacts.asMap().entrySet()) {
+			if(result.getValue().size()==0 && result.getKey()!=null) {
+				System.out.println(result.getKey()+": No Contacts Found");
+				break;
+			}
+
+			else if(result.getKey()!=null) {
+				emptyFlag=1;
+				System.out.println(result.getKey()+": "+result.getValue());
+			}
+		}
+		if(emptyFlag == 0) {
+			System.out.println("No Records Found");
+		}
 	}
 }
