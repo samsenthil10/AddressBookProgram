31448ad8e0329dc64b9f3b0b88892c287ef5a1e9
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-14 01:47:13 +0530

    Modified: To fix console menu navigation bug

--- src/main/java/com/bridgelabz/addressbookprogram/ContactsImpl.java
+++ src/main/java/com/bridgelabz/addressbookprogram/ContactsImpl.java
@@ -55,81 +55,82 @@ public class ContactsImpl implements ContactsIF {
 			System.out.print("Enter Phone Number: ");
 			String edit = AddressBookMain.scanner.next();
 			Contacts contact = null;
+			int exitFlag=0;
 			while(iterator.hasNext()) {
 				contact = iterator.next();
 				if(edit.equalsIgnoreCase(contact.getPhoneNumber())){
+					exitFlag=1;
+					System.out.println("Choose field to edit:\n[1] First Name\n[2] Last Name\n[3] Address\n[4] City\n[5] State\n[6] Pin Code\n[7] Phone Number\n[8] Email\n[9] Back");
+					int choice = AddressBookMain.scanner.nextInt();
+					switch(choice) {
+
+					case 1: System.out.print("Enter First Name: ");
+					contact.setFirstName(AddressBookMain.scanner.next());
+					System.out.println("First Name Successfully Edited");
 					break;
+					case 2: System.out.print("Enter Last Name: ");
+					contact.setLastName(AddressBookMain.scanner.next());
+					System.out.println("Last Name Successfully Edited");
+					break;
+					case 3: System.out.print("Enter Address: ");
+					contact.setAddress(AddressBookMain.scanner.next());
+					System.out.println("Address Successfully Edited");
+					break;
+					case 4: System.out.print("Enter City: ");
+					contact.setCity(AddressBookMain.scanner.next());
+					System.out.println("City Successfully Edited");
+					break;
+					case 5: System.out.print("Enter State: ");
+					contact.setState(AddressBookMain.scanner.next());
+					System.out.println("State Successfully Edited");
+					break;
+					case 6: System.out.print("Enter Pin Code: ");
+					contact.setZip(AddressBookMain.scanner.next());
+					System.out.println("Pin Code Successfully Edited");
+					break;
+					case 7: System.out.print("Enter Phone Number: ");
+					contact.setPhoneNumber(AddressBookMain.scanner.next());
+					System.out.println("Phone Number Successfully Edited");
+					break;
+					case 8: System.out.print("Enter E mail: ");
+					contact.setEmail(AddressBookMain.scanner.next());
+					System.out.println("Email Successfully Edited");
+					break;
+					case 9: break;
+					default: System.out.println("Invalid Choice");
+					}		
 				}
 			}
-			if(contact!=null) {
-
-				System.out.println("Choose field to edit:\n[1] First Name\n[2] Last Name\n[3] Address\n[4] City\n[5] State\n[6] Pin Code\n[7] Phone Number\n[8] Email\n[9] Back");
-				int choice = AddressBookMain.scanner.nextInt();
-				switch(choice) {
-
-				case 1: System.out.print("Enter First Name: ");
-				contact.setFirstName(AddressBookMain.scanner.next());
-				break;
-				case 2: System.out.print("Enter Last Name: ");
-				contact.setLastName(AddressBookMain.scanner.next());
-				break;
-				case 3: System.out.print("Enter Address: ");
-				contact.setAddress(AddressBookMain.scanner.next());
-				break;
-				case 4: System.out.print("Enter City: ");
-				contact.setCity(AddressBookMain.scanner.next());
-				break;
-				case 5: System.out.print("Enter State: ");
-				contact.setState(AddressBookMain.scanner.next());
-				break;
-				case 6: System.out.print("Enter Pin Code: ");
-				contact.setZip(AddressBookMain.scanner.next());
-				break;
-				case 7: System.out.print("Enter Phone Number: ");
-				contact.setPhoneNumber(AddressBookMain.scanner.next());
-				break;
-				case 8: System.out.print("Enter E mail: ");
-				contact.setEmail(AddressBookMain.scanner.next());
-				break;
-				case 9: System.exit(0);
-				break;
-				default: System.out.println("Invalid Choice");
-				}
-			}
-			else {
-
-				System.out.println("Contact Not Found!");
-			}
+			if(exitFlag==0)
+				System.out.println("Contact Not Found");
 		}
-
 	}
 
 	@Override
 	public void deleteContact(AddressBook addressBook) {
 
 		Iterator<Contacts> iterator = addressBook.contacts.iterator();
-		if(iterator.hasNext() == false) {
-			System.out.println("Contacts empty!");
-		}		
+		if(iterator.hasNext()==false) {
+			System.out.println("Contacts Empty!");
+		}
 		else {
 			System.out.print("Enter Phone Number: ");
 			String delete = AddressBookMain.scanner.next();
-
 			Contacts contact = null;
+			int exitFlag=0;
 			while(iterator.hasNext()) {
 				contact = iterator.next();
 				if(delete.equalsIgnoreCase(contact.getPhoneNumber())){
+					exitFlag=1;
+					addressBook.contacts.remove(contact);
+					System.out.println("Contact Successfully Deleted");
 					break;
 				}
 			}
-			if(contact!=null) {
-				addressBook.contacts.remove(contact);
-			}
-			else {
-
-				System.out.println("Contact Not Found!");
-			}
+			if(exitFlag==0)
+				System.out.println("Contact Not Found");
 		}
+
 	}
 
 	@Override
