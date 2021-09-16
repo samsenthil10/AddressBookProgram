007c25049e1fb081b4bfef547464150d9143481f
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-16 05:32:59 +0530

    Refactored: To see contacts in all cities/states using stream

--- src/main/java/com/bridgelabz/addressbookprogram/AddressBookImpl.java
+++ src/main/java/com/bridgelabz/addressbookprogram/AddressBookImpl.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.addressbookprogram;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -44,90 +45,89 @@ public class AddressBookImpl implements AddressBookIF {
 	public void showContactsInGivenCity(String enteredCity, Map<String, AddressBook> addressBooks) {
 
 		System.out.println("City: "+enteredCity);
-		List<Contacts> contactsInCity = null;
+		List<Contacts> contactsInCity = new ArrayList<>();
 		for (Entry<String, AddressBook> entry : addressBooks.entrySet()) {
-			contactsInCity = entry.getValue().contacts.stream()
-					.filter(contact -> contact.getState().equalsIgnoreCase(enteredCity))
-					.collect(Collectors.toList());
+			entry.getValue().contacts.stream()
+			.filter(contact -> contact.getState().equalsIgnoreCase(enteredCity))
+			.forEach(contactsInCity::add);
 		}
 		if(contactsInCity.size()>0) {
 			contactsInCity.stream()
 			.forEach(contact -> {System.out.println(contact.getFirstName()+" "+contact.getLastName());});
 		}	
+		else {
+			System.out.println("No Contacts Found!");
+		}
 	}
 	@Override
 	public void showContactsInGivenState(String enteredState, Map<String, AddressBook> addressBooks) {
 
 		System.out.println("State: "+enteredState);
-		List<Contacts> contactsInState = null;
+		List<Contacts> contactsInState = new ArrayList<>();
 		for (Entry<String, AddressBook> entry : addressBooks.entrySet()) {
-			contactsInState = entry.getValue().contacts.stream()
-					.filter(contact -> contact.getState().equalsIgnoreCase(enteredState))
-					.collect(Collectors.toList());
+			entry.getValue().contacts.stream()
+			.filter(contact -> contact.getState().equalsIgnoreCase(enteredState))
+			.forEach(contactsInState::add);
 		}
 		if(contactsInState.size()>0) {
 			contactsInState.stream()
 			.forEach(contact -> {System.out.println(contact.getFirstName()+" "+contact.getLastName());});
 		}	
+		else {
+			System.out.println("No Contacts Found!");
+		}
 	}
 
 	@Override
 	public void showAllContactsInAllCity(Map<String, AddressBook> addressBooks) {
-		int emptyFlag =0;
+
+		List<Contacts> contactsInCity = new ArrayList<>();
 		Multimap<String, String> cityContacts = ArrayListMultimap.create();
 		for (Entry<String, AddressBook> entry : addressBooks.entrySet()) {
-			Iterator<Contacts> iterator = entry.getValue().contacts.iterator();
-			while(iterator.hasNext()) {
-				Contacts contact = iterator.next();
-				if(contact == null)
-					continue;
-				cityContacts.put(contact.getCity(),contact.getFirstName()+" "+contact.getLastName());
-			}					
+			entry.getValue().contacts.stream()
+			.forEach(contactsInCity::add);
 		}
-		for(Entry<String,Collection<String>> result : cityContacts.asMap().entrySet()) {
-			if(result.getValue().size()==0 && result.getKey()!=null) {
-				System.out.println(result.getKey()+": No Contacts Found");
-				break;
-			}
+		contactsInCity.stream()
+		.forEach(contact -> {cityContacts.put(contact.getCity(), contact.getFirstName()+" "+contact.getLastName());});
+		if(cityContacts.size()>0) {
+			for(Entry<String,Collection<String>> result : cityContacts.asMap().entrySet()) {
+				if(result.getValue().size()==0 && result.getKey()!=null) {
+					System.out.println(result.getKey()+": No Contacts Found");
+					break;
+				}
 
-			else if(result.getKey()!=null) {
-				emptyFlag=1;
-				System.out.println("City "+result.getKey()+": Count :"+result.getValue().size()+" Contacts: "+result.getValue());
+				else if(result.getKey()!=null) {
+					System.out.println("City "+result.getKey()+" Contacts: "+result.getValue());
+				}
 			}
 		}
-		if(emptyFlag == 0) {
+		else
 			System.out.println("No Records Found");
-		}
 	}
-
 	@Override
 	public void showAllContactsInAllState(Map<String, AddressBook> addressBooks) {
-
-		int emptyFlag=0;
+		List<Contacts> contactsInState = new ArrayList<>();
 		Multimap<String, String> stateContacts = ArrayListMultimap.create();
 		for (Entry<String, AddressBook> entry : addressBooks.entrySet()) {
-			Iterator<Contacts> iterator = entry.getValue().contacts.iterator();
-			while(iterator.hasNext()) {
-				Contacts contact = iterator.next();
-				if(contact == null)
-					continue;
-				stateContacts.put(contact.getState(),contact.getFirstName()+" "+contact.getLastName());
-			}					
+			entry.getValue().contacts.stream()
+			.forEach(contactsInState::add);
 		}
-		for(Entry<String,Collection<String>> result : stateContacts.asMap().entrySet()) {
-			if(result.getValue().size()==0 && result.getKey()!=null) {
-				System.out.println(result.getKey()+": No Contacts Found");
-				break;
-			}
+		contactsInState.stream()
+		.forEach(contact -> {stateContacts.put(contact.getState(), contact.getFirstName()+" "+contact.getLastName());});
+		if(stateContacts.size()>0) {
+			for(Entry<String,Collection<String>> result : stateContacts.asMap().entrySet()) {
+				if(result.getValue().size()==0 && result.getKey()!=null) {
+					System.out.println(result.getKey()+": No Contacts Found");
+					break;
+				}
 
-			else if(result.getKey()!=null) {
-				emptyFlag=1;
-				System.out.println("State "+result.getKey()+": Count :"+result.getValue().size()+" Contacts: "+result.getValue());
+				else if(result.getKey()!=null) {
+					System.out.println("State "+result.getKey()+" Contacts: "+result.getValue());
+				}
 			}
 		}
-		if(emptyFlag == 0) {
+		else
 			System.out.println("No Records Found");
-		}
 	}
 
 	@Override
